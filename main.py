f89a706e46d99ae3af344fa76c83e3d33bd32558
Alexandr-ace <syakubin@bk.ru>
2026-06-06 10:38:48 +0300

    Мелкие изменения

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+# venv\Scripts\activate.bat для виндовс
+# source venv/bin/activate для мака
+# pip install -r requirements.txt
 # Начни с базового скрипта
 import requests
 from bs4 import BeautifulSoup
